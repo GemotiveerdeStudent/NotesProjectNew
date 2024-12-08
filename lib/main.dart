c306218219bead464e23ee86f719f0b9aa269914
GemotiveerdeStudent <1453653lasschuit@zuyd.com>
2024-12-08 19:11:24 +0100

    Commit merge

--- lib/main.dart
+++ lib/main.dart
@@ -1,21 +1,20 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart';
-import 'package:notes/views/login_view.dart';
 import 'firebase_options.dart';
 
 void main() {
   WidgetsFlutterBinding.ensureInitialized();
 
   runApp(MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(
-          seedColor: const Color.fromARGB(255, 96, 150, 219)),
-        useMaterial3: true,
-      ),
-      home: const HomePage(),
-    ));
+    title: 'Flutter Demo',
+    theme: ThemeData(
+      colorScheme: ColorScheme.fromSeed(
+        seedColor: const Color.fromARGB(255, 96, 150, 219)),
+      useMaterial3: true,
+    ),
+    home: const HomePage(),
+  ));
 }
 
 class HomePage extends StatelessWidget {
@@ -27,26 +26,32 @@ class HomePage extends StatelessWidget {
       appBar: AppBar(
         title: const Text('Home'),
         backgroundColor: Colors.blue,
-       ),
+      ),
       body: FutureBuilder(
         future: Firebase.initializeApp(
           options: DefaultFirebaseOptions.currentPlatform,
         ),
         builder: (context, snapshot) {
+          print('Connection state: ${snapshot.connectionState}');
+          if (snapshot.hasError) {
+            print('Error: ${snapshot.error}');
+            return const Text('Error initializing Firebase');
+          }
           switch (snapshot.connectionState) {
             case ConnectionState.done:
               final user = FirebaseAuth.instance.currentUser;
               final emailVerified = user?.emailVerified ?? false;
-              if (user?.emailVerified ?? false) {
-                print(' You are a verified user');
+              if (emailVerified) {
+                print('You are a verified user');
               } else {
                 print('You are not a verified user, you need to verify!');
+                print(user);
               }
               return const Text('Done');
             default:
               return const Text('Loading...');
           }
-        },  
+        },
       ),
     );
   }
