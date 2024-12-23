393fbd42cfaa600493eee0438c668efc7a2f4010
GemotiveerdeStudent <1453653lasschuit@Zuyd.nl>
2024-12-23 15:57:42 +0100

    Step-16 , Dialog rewritten to generic dialog, Split all under utilities. Added delete to Frontend

--- lib/views/login_view.dart
+++ lib/views/login_view.dart
@@ -1,8 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:notes/services/auth/auth_exceptions.dart';
 import 'package:notes/services/auth/auth_service.dart';
+import 'package:notes/utilities/dialogs/error_dialog.dart';
 import '../constants/routes.dart';
-import '../utilities/show_error_dialog.dart';
 
 class LoginView extends StatefulWidget {
   const LoginView({super.key});
--- lib/views/notes/notes_view.dart
+++ lib/views/notes/notes_view.dart
@@ -3,6 +3,8 @@ import 'package:notes/constants/routes.dart';
 import 'package:notes/enums/menu_action.dart';
 import 'package:notes/services/auth/auth_service.dart';
 import 'package:notes/services/crud/notes_service.dart';
+import 'package:notes/utilities/dialogs/logout_dialog.dart';
+import 'package:notes/views/notes/notes_list_view.dart';
 
 class NotesView extends StatefulWidget {
   const NotesView({super.key});
@@ -74,22 +76,13 @@ class _NotesViewState extends State<NotesView> {
                         case ConnectionState.waiting:
                         case ConnectionState.active:
                           if (snapshot.hasData) {
-                            final allNotes = snapshot.data as List<DatabaseNote>;
-                            print(allNotes);
-                            return ListView.builder(
-                              itemCount: allNotes.length,
-                              itemBuilder: (context, index) {
-                                final note = allNotes[index];
-                                return ListTile(
-                                  title: Text(
-                                    note.text,
-                                    maxLines: 1,
-                                    softWrap: true,
-                                    overflow: TextOverflow.ellipsis,
-                                  ),
-                                  );
-                              },
-                            );
+                            final allNotes =
+                                snapshot.data as List<DatabaseNote>;
+                            return NotesListView(
+                                notes: allNotes,
+                                onDeleteNote: (note) async {
+                                  await _notesService.deleteNote(id: note.id);
+                                });
                           } else {
                             return const CircularProgressIndicator();
                           }
@@ -103,29 +96,4 @@ class _NotesViewState extends State<NotesView> {
           },
         ));
   }
-}
-
-Future<bool> showLogOutDialog(BuildContext context) {
-  return showDialog<bool>(
-      context: context,
-      builder: (context) {
-        return AlertDialog(
-          title: const Text('Log out'),
-          content: const Text('Are you sure you want to log out?'),
-          actions: [
-            TextButton(
-              onPressed: () {
-                Navigator.of(context).pop(false);
-              },
-              child: const Text('Cancel'),
-            ),
-            TextButton(
-              onPressed: () {
-                Navigator.of(context).pop(true);
-              },
-              child: const Text('Log out'),
-            ),
-          ],
-        );
-      }).then((value) => value ?? false);
 }
